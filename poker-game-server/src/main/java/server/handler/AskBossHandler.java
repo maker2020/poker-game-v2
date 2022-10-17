82c00cbc6523f7c01e40b02c6a58c78a0e07ffbd
hufangshuai <1838813391@qq.com>
2022-10-17 16:44:54 +0800

    待完善叫地主抢地主逻辑

--- poker-game-server/src/main/java/server/handler/AskBossHandler.java
+++ poker-game-server/src/main/java/server/handler/AskBossHandler.java
@@ -1,11 +1,19 @@
 package server.handler;
 
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
+
 import game.dto.AskBossDTO;
 import game.entity.Player;
 import game.entity.Room;
+import game.enums.ActionEnum;
+import game.vo.Notification;
+import game.vo.ResultVO;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import server.handler.holder.ChannelHolder;
 
 /**
@@ -19,6 +27,16 @@ public class AskBossHandler extends SimpleChannelInboundHandler<AskBossDTO>{
         Player player = ChannelHolder.attrPlayer(ctx.channel());
         ChannelGroup group = ChannelHolder.groupMap.get(ctx.channel());
         
+        player.setReqIndex(room.getTurnCallIndex().get());
+        if(msg.isTendency()){
+            player.reqBoss();
+            Map<String,Object> result=ResultVO.resultMap(ActionEnum.ASK, room.turnPlayer(player).getName(), new Notification(ActionEnum.ASK, true, player.getName()));
+            group.writeAndFlush(new TextWebSocketFrame(JSON.toJSONString(result)));
+        }else{
+            player.refuseBoss();
+            Map<String,Object> result=ResultVO.resultMap(ActionEnum.ASK, room.turnPlayer(player).getName(), new Notification(ActionEnum.ASK, false, player.getName()));
+            group.writeAndFlush(new TextWebSocketFrame(JSON.toJSONString(result)));
+        }
     }
     
 }
