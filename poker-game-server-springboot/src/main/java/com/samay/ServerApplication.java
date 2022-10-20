f06eb02faca13c8c0be7130d3b38133aec1ba2ca
Samay <1838813391@qq.com>
2022-10-20 20:32:10 +0800

    修改mysql依赖、修改jvm参数(配置文件环境)、新增多个环境配置类。

--- poker-game-server-springboot/src/main/java/com/samay/ServerApplication.java
+++ poker-game-server-springboot/src/main/java/com/samay/ServerApplication.java
@@ -1,9 +1,11 @@
 package com.samay;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.samay.dao")
 public class ServerApplication {
 
     public static void main(String[] args) {
