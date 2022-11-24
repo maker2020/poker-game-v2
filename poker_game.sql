e2e98e75724c4b2d7f3ae969701d46326085a08c
hufangshuai <1838813391@qq.com>
2022-11-24 16:37:58 +0800

    数据库结构上传

--- poker_game.sql
+++ poker_game.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 20/10/2022 16:18:12
+ Date: 24/11/2022 16:37:31
 */
 
 SET NAMES utf8mb4;
@@ -22,7 +22,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `item`;
 CREATE TABLE `item`  (
-  `userid` int(0) NOT NULL COMMENT '用户唯一主键',
+  `userid` char(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户唯一主键',
   `name` enum('POKER_COUNTER','SUPER_DOUBLED') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '道具名称/类别',
   `count` int(0) NULL DEFAULT NULL COMMENT '道具数量',
   PRIMARY KEY (`userid`, `name`) USING BTREE,
@@ -34,16 +34,31 @@ CREATE TABLE `item`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT COMMENT '用户唯一主键',
+  `id` char(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户唯一主键',
   `phone` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号',
-  `nickname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户昵称',
+  `avatarUrl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户头像地址',
+  `nickName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户昵称',
   `sex` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '性别',
+  `country` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户所在国家',
+  `province` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户所在省份',
+  `city` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户所在城市',
+  `language` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户所用语言',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for user_data
+-- ----------------------------
+DROP TABLE IF EXISTS `user_data`;
+CREATE TABLE `user_data`  (
+  `userid` char(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '玩家id(主,外键)',
   `freeMoney` bigint(0) NULL DEFAULT 5000 COMMENT '游戏普通货币',
   `payMoney` bigint(0) NULL DEFAULT NULL COMMENT '游戏收费货币',
   `winCount` bigint(0) NULL DEFAULT NULL COMMENT '玩家普通游戏胜场',
   `loseCount` bigint(0) NULL DEFAULT NULL COMMENT '玩家普通游戏败场',
   `exp` bigint(0) NULL DEFAULT NULL COMMENT '玩家经验值',
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`userid`) USING BTREE,
+  CONSTRAINT `data_clear` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -52,8 +67,19 @@ CREATE TABLE `user`  (
 DROP TRIGGER IF EXISTS `init_item`;
 delimiter ;;
 CREATE TRIGGER `init_item` AFTER INSERT ON `user` FOR EACH ROW begin
-insert into user_item values(new.id,'POKER_COUNTER',3);
-insert into user_item values(new.id,'SUPER_DOUBLED',3);
+insert into item values(new.id,'POKER_COUNTER',3);
+insert into item values(new.id,'SUPER_DOUBLED',3);
+END
+;;
+delimiter ;
+
+-- ----------------------------
+-- Triggers structure for table user
+-- ----------------------------
+DROP TRIGGER IF EXISTS `init_data`;
+delimiter ;;
+CREATE TRIGGER `init_data` AFTER INSERT ON `user` FOR EACH ROW begin
+insert into user_data values(new.id,5000,0,0,0,0);
 END
 ;;
 delimiter ;
